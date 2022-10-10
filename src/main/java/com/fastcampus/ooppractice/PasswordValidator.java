13b21261a1c3d2ca1f19b2c980ed751ac51f37cf
qkrthdud002 <qkrthdud002@gmail.com>
2022-10-10 23:46:56 +0900

    MVC 프레임워크 1010

--- src/main/java/com/fastcampus/ooppractice/PasswordValidator.java
+++ src/main/java/com/fastcampus/ooppractice/PasswordValidator.java
@@ -3,5 +3,10 @@ package com.fastcampus.ooppractice;
 public class PasswordValidator {
 
     public static void validate(String password) {
+
+        // 올바르지 않게 짬.
+        if (password.length() < 8 || password.length() > 12) {
+            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+        }
     }
 }
