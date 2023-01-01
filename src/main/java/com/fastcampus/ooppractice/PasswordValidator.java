386b8dfeae09af8081592126b97c84744bb456a2
qkrthdud002 <qkrthdud002@gmail.com>
2023-01-01 23:16:03 +0900

    MVC 프레임워크 0101

--- src/main/java/com/fastcampus/ooppractice/PasswordValidator.java
+++ src/main/java/com/fastcampus/ooppractice/PasswordValidator.java
@@ -2,11 +2,15 @@ package com.fastcampus.ooppractice;
 
 public class PasswordValidator {
 
+    private static final String WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE = "비밀번호는 최소 8자 이상 12자 이하여야 한다.";
+
     public static void validate(String password) {
 
         // 올바르지 않게 짬.
-        if (password.length() < 8 || password.length() > 12) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+        int length = password.length();
+        
+        if (length < 8 || length > 12) {
+            throw new IllegalArgumentException(WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 }
--- src/test/java/com/fastcampus/ooppractice/OopPracticeApplicationTests.java
+++ src/test/java/com/fastcampus/ooppractice/OopPracticeApplicationTests.java
@@ -2,6 +2,8 @@ package com.fastcampus.ooppractice;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -11,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThatCode;
  * 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다.
  * 경계조건에 대해 테스트 코드를 작성해야 한다.
  * */
-
 //@SpringBootTest
 public class OopPracticeApplicationTests {
 
@@ -22,5 +23,14 @@ public class OopPracticeApplicationTests {
 				.doesNotThrowAnyException();
 	}
 
+	@DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+//	@Test
+	void validatePasswordTest2(String password) {
+		assertThatCode(() -> PasswordValidator.validate(password))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+	}
 
 }
